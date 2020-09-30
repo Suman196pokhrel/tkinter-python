51406f5f9368fdd94cb7efd977fc6b86446c3ffc
Suman_pokhrel <spokhrel196@gmail.com>
2020-09-30 19:06:07 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,25 @@
-from tkinter import*
-root=Tk()
-equation=StringVar()
-calculation=Label(root,text=equation)
-equation.set("kfldgi;jrdof;l")
-calculation.grid(columnspan=4)
-root.mainloop()
+from tkinter import *
+
+
+class test(Tk):
+    def __init__(self, width, height):
+        super().__init__()
+        # Initial Variable Declaration and Assignment
+
+        self.width = width
+        self.height = height
+        self.wm_geometry(f'{self.width}x{self.height}')
+        self.title('Test App')
+        self.equation = StringVar()
+
+        # Widgets Creation and Placement
+        self.calculation = Label(self, text=self.equation)
+        self.calculation.grid(columnspan=4)
+
+        self.equation.set("kfldgi;jrdof;l")
+
+
+if __name__ == '__main__':
+    mainwin = test(250, 120)
+
+    mainwin.mainloop()
